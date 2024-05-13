27371cb247f5686ca5126ff15d6fc07988f30e65
Nathan Lane <lanenathan214@gmail.com>
2024-05-13 14:34:47 -0400

    added comments to homepage route to move fetch request functions

--- controllers/views/index.js
+++ controllers/views/index.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.render("homepage")
+    // we COULD make the API request for data in our ROUTE/CONTROLLER
+
+    // we can parse that returned data (images, title)
+
+    // we can SEND the DATA in the CONTEXT OBJECT with the VIEW
+
+    let data = { title: "Bingo", username: "Bob" }
+    res.render("homepage", { viewData: data })
 })
 
 router.get("/login", (req, res) => {
