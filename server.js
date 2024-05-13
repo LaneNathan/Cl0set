30e00ab28cdcf018740e4f23f36c8957a43ebfc6
Nathan Lane <lanenathan214@gmail.com>
2024-05-13 14:33:43 -0400

    created a constant for hbs

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 const sess = {
     secret: 'Super secret secret',
