063260f576085d02bf0fb7c809c6f450189ef476
Nathan Lane <lanenathan214@gmail.com>
2024-05-13 14:32:38 -0400

    commented out helpers until we make a helpers until we create file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
