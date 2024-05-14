103ab1f32d17b94271d573d91d3077c3e775141a
Nathan Lane <lanenathan214@gmail.com>
2024-05-14 19:50:00 -0400

    added the Fetchforever21Data function to index.js

--- controllers/views/index.js
+++ controllers/views/index.js
@@ -29,7 +29,46 @@ let search = 'mens-shirts';
 router.get("/login", (req, res) => {
    
     res.render("login")
-})
+});
 
+ // make the API request for data in our ROUTE/CONTROLLER
+/*async function fetchAsosData(search){
+    const options = {
+      method: 'GET',
+      url: 'https://asos-com1.p.rapidapi.com/products/search',
+      params: {
+        q: search
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+        'X-RapidAPI-Host': process.env.RAPIDAPI_HOST,
+      }
+    };
+
+    const response = await axios.request(options);
+    return response.data;
+}*/
+
+async function FetchForever21Data(search){
+    const options = {
+      method: 'GET',
+      url: 'https://apidojo-forever21-v1.p.rapidapi.com/products/search',
+      params: {
+        query: search,
+        rows: '10',
+        start: '0'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.FOREVER21_API_KEY,
+        'X-RapidAPI-Host': process.env.FOREVER21_API_HOST
+      }
+    };
+
+    const response = await axios.request(options);
+
+
+    console.log(response.data);
+    return response.data;
+}
 
 module.exports = router;
