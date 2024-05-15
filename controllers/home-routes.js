b8607b9d1080af46d8587c774e193950dfdb7785
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-05-14 23:02:55 -0400

    user routeslogin, sessions, user IDs

---
+++ controllers/home-routes.js
@@ -0,0 +1,12 @@
+// Login route
+router.get('/sign-up', (req, res) => {
+    // If the user is already logged in, redirect to the homepage
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    // Otherwise, render the 'login' template
+    res.render('sign-up');
+  });
+  
+  module.exports = router;
--- controllers/views/index.js
+++ controllers/views/index.js
@@ -1,4 +1,13 @@
+const { User } = require("../../models");
+const express = require('express');
+
 const router = require("express").Router();
+
+const app = express();
+const PORT = 3001;
+
+
+
 
 router.get("/", (req, res) => {
     // we COULD make the API request for data in our ROUTE/CONTROLLER
@@ -7,14 +16,35 @@ router.get("/", (req, res) => {
 
     // we can SEND the DATA in the CONTEXT OBJECT with the VIEW
 
-    let data = { title: "Bingo", username: "Bob" }
-    res.render("homepage", { viewData: data })
+    // let data = { title: "Bingo", username: "Bob" }
+    // res.render("homepage", { viewData: data })
 })
 
-router.get("/login", (req, res) => {
-   
-    res.render("login")
-})
+// Login route
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = User.find(u => req.body.username === username && req.body.password === password);
+    let signIn = false;
 
+    if (user) {
+        res.status(200).json({ message: 'Login successful', user });
+        let signedIn = true;
+        console.log(signedIn);
+
+    } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+    }
+});
+
+// Home route Do we need?
+router.get('/', (req, res) => {
+    res.send('Welcome to the home page');
+});
+
+app.use('/', router); // Using router in the application
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = router;
