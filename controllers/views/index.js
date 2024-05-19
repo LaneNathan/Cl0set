af885ea82c3bbec1e688352274636e957e21b8f3
Nathan Lane <lanenathan214@gmail.com>
2024-05-19 17:10:59 -0400

    deleted some comments and open space

--- controllers/views/index.js
+++ controllers/views/index.js
@@ -134,40 +134,8 @@ module.exports = router;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  // make the API request for data in our ROUTE/CONTROLLER
-/*async function fetchAsosData(search){
-    const options = {
-      method: 'GET',
-      url: 'https://asos-com1.p.rapidapi.com/products/search',
-      params: {
-        q: search
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': process.env.RAPIDAPI_HOST,
-      }
-    };
 
-    const response = await axios.request(options);
-    return response.data;
-}*/
 //---------------------------Original Position Nate-------------------//
 
 
